fc94f665061f8127621cdb4640c5215ed513ac2d
Arvind Rana <rana.webdeveloper@gmail.com>
2026-06-18 11:08:32 +0530

    fix: add root route redirect

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,23 @@
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import MainLayout from "../layout/MainLayout";
 import UserDetails from "../pages/UserDetails";
 
-export default function AppRoutes (){
-    return(
-            <Routes>
-             <Route element={<MainLayout />}>
-                 <Route path="/home" element={<Home />} />
-                 <Route path="/products" element={<Products />} />
-                 <Route path="/users/:id" element={<UserDetails />} />
-             </Route>
-            </Routes>
-    )
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+
+        {/* Root route (Vercel fix) */}
+        <Route path="/" element={<Navigate to="/home" />} />
+
+        {/* Pages */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+
+      </Route>
+    </Routes>
+  );
 }
